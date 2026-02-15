2e8bafa249c8f6aa65e40703f112898eae12024a
Reggy Gaines <reggy.gaines@gmail.com>
2026-02-15 18:51:14 -0500

    Added [AddColor] API + hide keys

---
+++ colors-lamp/api/AddColor.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$color = $inData["color"];
+	$userId = $inData["userId"];
+
+	require_once "../config.php";
+
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+		$stmt->bind_param("ss", $userId, $color);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
